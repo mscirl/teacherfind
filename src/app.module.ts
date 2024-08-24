cc3c6f339d01fdc5af2413166b9f4c3ed2e2c706
Yara <yaralviana@gmail.com>
2024-08-24 14:26:30 -0300

    configurações de db #5

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TeacherModule } from './teacher/teacher.module';
+import { TypeOrmModule } from '@nestjs/typeorm'; 
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [TeacherModule],
+    imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',  
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [], //Adicionar aqui as entidades
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([TeacherModule]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
